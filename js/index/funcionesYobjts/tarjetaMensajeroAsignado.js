1aa02ab7ea7a8fd3cf871062e63d6f87de94df2a
HenryDelaossa <delaossah@gmail.com>
2022-01-30 13:46:35 -0500

    stable

--- js/index/funcionesYobjts/tarjetaMensajeroAsignado.js
+++ js/index/funcionesYobjts/tarjetaMensajeroAsignado.js
@@ -3,15 +3,14 @@ function tarjetaMensajeroAsignado(contenedor, direccion1) {
     let indiceAleat = Math.floor((Math.random() * (5 - 0)) + 0);
     spinner("#body");
     setTimeout(()=> {
-        let contspiner = document.querySelector(".contspinner");
-        contspiner.style.display = "none"
+        $(".contspinner").hide();
         $(contenedor).append(`<div class="conTarjetaMens">
                                 <div>
                                     <p class="pinfoExt" style=" height: auto; background-color: #d2d2d2; margin-right: 10px;"> Tu solicitud ah sido exitosa, a continuacion los datos de el mejor logimensajero encargado de realizar tu servicio. </p>
                                     <p class="spanfoExt" style=" height: auto; background-color: #171717;color: #d2d2d2; margin: 10px 10px 0 0;  font-size: 10px">En menos de 30 minutos llegara a la direccion uno: ${direccion1.toUpperCase()}, recuerda comunicarte con tu mensajero asignado para mantener mejor comunicacion y coordinacion</p>
                                 </div>
                                 <div class="divNombreFoto" style=" margin-right: 10px;">
-                                    <p class="pNombreMens">${mensajeros[indiceAleat].nombreMens.toUpperCase()} ${mensajeros[indiceAleat].apellidoMens.toUpperCase()}</p>
+                                    <p class="pNomgitbreMens">${mensajeros[indiceAleat].nombreMens.toUpperCase()} ${mensajeros[indiceAleat].apellidoMens.toUpperCase()}</p>
                                     <p class="imgMens"><img src="${mensajeros[indiceAleat].fotoMens}" alt="foto Mensajero Asignado Cologista"></p>
                                 </div>
                                 <div class="divInfoMens">
@@ -20,12 +19,12 @@ function tarjetaMensajeroAsignado(contenedor, direccion1) {
                                     <p class="pTelMens"> Telefono: <span class="spanInfMn">${mensajeros[indiceAleat].telefonoMens}</span></p>
                                 </div>
                             </div>`);
-        // doy estilos dinamicamente a contenedor y contenedor de tarjeta con info de mensajero
-        $(contenedor).css({"z-index": "1000",width: "100%", height: "100%", position: "fixed", top:"0", left:"0", "background-color": "#17171750"}).slideDown(500)
-        $(".conTarjetaMens").css({width: "60%", position:"absolute", top:"50%", left:"50%", transform:"translate(-50%,-50%)","background-color": "#da0039", display: "flex"/*, "flex-direction": "column"*/,"justify-content": "space-evenly", "align-items": "center"});
-        $(".spanInfMn, .spanfoExt").css({display: "block"})
-        // alaceno datos en localstorage
-        localStorage.setItem("LastMensAsign", JSON.stringify(mensajeros[indiceAleat]));
+    // doy estilos dinamicamente a contenedor y contenedor de tarjeta con info de mensajero
+    $(contenedor).css({"z-index": "1000",width: "100%", height: "100%", position: "fixed", top:"0", left:"0", "background-color": "#17171750"}).slideDown(500)
+    $(".conTarjetaMens").css({width: "60%", position:"absolute", top:"50%", left:"50%", transform:"translate(-50%,-50%)","background-color": "#da0039", display: "flex"/*, "flex-direction": "column"*/,"justify-content": "space-evenly", "align-items": "center"});
+    $(".spanInfMn, .spanfoExt").css({display: "block"})
+    // alaceno datos en localstorage
+    localStorage.setItem("LastMensAsign", JSON.stringify(mensajeros[indiceAleat]));
     },1500);
     // programo evento para cerrar ventana de info de mensajero asignado
     const conten = document.querySelector(contenedor)
@@ -33,4 +32,5 @@ function tarjetaMensajeroAsignado(contenedor, direccion1) {
         if (evt.target === conten) {$(contenedor).slideUp();$(".pNombreMens, .imgMens, .pVehicu, .pPlacaVehic, .pTelMens, .pinfoExt").text("")
         }
     }); 
+    
 };
